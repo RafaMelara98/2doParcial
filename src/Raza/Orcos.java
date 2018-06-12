de84b31fbdb5c844cf56109e0cc8dcf7168d5db4
Rafael Melara <Rafael Melara@DESKTOP-ICRJ4VN>
2018-06-12 17:11:23 -0600

    Se crearon razas

---
+++ src/Raza/Orcos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Raza;
+
+/**
+ *
+ * @author Rafael Melara
+ */
+public class Orcos implements raza {
+
+    @Override
+    public void Crear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
