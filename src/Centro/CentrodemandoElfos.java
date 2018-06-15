4ce277cc386ec422baaff263a1fbc91dd1bc774f
Rafael Melara <Rafael Melara@DESKTOP-ICRJ4VN>
2018-06-14 20:16:24 -0600

    Creando centro de mando

---
+++ src/Centro/CentrodemandoElfos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Centro;
+
+import Recursos.recurso;
+
+/**
+ *
+ * @author Rafael Melara
+ */
+public class CentrodemandoElfos implements centro {
+    private int vida = 500;
+
+    @Override
+    public void Crear() {
+        recurso s = null;
+        System.out.println("Centro de mando iniciado");
+        
+    }
+    
+}
---
+++ src/Centro/CentrodemandoOrcos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Centro;
+
+import Recursos.recurso;
+
+/**
+ *
+ * @author Rafael Melara
+ */
+public class CentrodemandoOrcos implements centro {
+    
+     private int vida = 500;
+
+    @Override
+    public void Crear() {
+        recurso s = null;
+        System.out.println("Centro de mando iniciado");
+        
+    }
+    
+    
+    
+}
